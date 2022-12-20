7b4b8cd884f5a0042cf677f03c156e643ee14011
Sebastian Speitel <sebastian.speitel@outlook.de>
2022-12-20 21:44:33 +0100

    cli/init: append new repo to config
    When initializing a repo borrg now appends it to the config,
    so it gets run automatically

--- src/cli/init.rs
+++ src/cli/init.rs
@@ -30,8 +30,10 @@ pub fn init(borg: Borg, config: Config, args: Args) {
     // Search matching backup in config
     let backup = config.backups.iter().map(|(r, _)| r).find(|r| r == &&repo);
 
+    let mut exists_already = false;
     if let Some(backup) = backup {
         repo.passphrase = backup.passphrase.clone();
+        exists_already = true;
     }
 
     if let Err(e) = borg.init_repository::<backend::borg::BorgWrapper>(
@@ -46,5 +48,60 @@ pub fn init(borg: Borg, config: Config, args: Args) {
     ) {
         eprintln!("Failed to initialize repository: {}", e);
         std::process::exit(1);
+    }
+
+    if !exists_already {
+        if let Err(e) = append_backup_config(&config.source, &repo) {
+            eprintln!("Failed to append backup to config: {}", e);
+        }
+    }
+}
+
+fn append_backup_config(
+    path: &std::path::PathBuf,
+    repo: &crate::Repo,
+) -> Result<(), std::io::Error> {
+    use std::fs::OpenOptions;
+    use std::io::Write;
+
+    let mut file = OpenOptions::new().append(true).open(path)?;
+
+    file.write(b"\n[[backup]]\nrepository = \"")?;
+    file.write(repo.to_string().as_bytes())?;
+    file.write(b"\"\n")?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_init() {
+        let args = super::Args {
+            encryption: Encryption::None,
+            append_only: false,
+            storage_quota: None,
+            make_parent_dirs: false,
+            repository: "/tmp/test-repo".parse().unwrap(),
+        };
+
+        let config_path = std::path::PathBuf::from("/tmp/borrg.toml");
+
+        // Cleanup
+        std::fs::remove_file(&config_path).ok();
+        std::fs::remove_dir_all("/tmp/test-repo").ok();
+
+        std::fs::write(&config_path, "").unwrap();
+
+        let borg = Borg::default();
+        let config = Config::load(&config_path).unwrap();
+
+        init(borg, config, args);
+
+        let config_after = Config::load(&config_path).unwrap();
+        assert_eq!(config_after.backups.len(), 1);
     }
 }
