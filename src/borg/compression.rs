205bf9df29b10dd23ad21ac25c52f8121663ea57
Sebastian Speitel <sebastian.speitel@fem.tu-ilmenau.de>
2025-04-07 13:31:42 +0200

    chore: follow clippy lints

--- src/borg/compression.rs
+++ src/borg/compression.rs
@@ -2,6 +2,142 @@ use std::str::FromStr;
 
 use smol_str::SmolStr;
 
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
+pub enum Compression {
+    None,
+    #[default]
+    Lz4,
+    Lz4Auto,
+    Zstd(Zstd),
+    ZstdAuto(Zstd),
+    Zlib(Zlib),
+    ZlibAuto(Zlib),
+    Lzma(Lzma),
+    LzmaAuto(Lzma),
+}
+
+impl Compression {
+    #[inline]
+    #[must_use]
+    pub const fn is_auto(&self) -> bool {
+        matches!(
+            self,
+            Self::Lz4Auto | Self::LzmaAuto(..) | Self::ZstdAuto(..) | Self::ZlibAuto(..)
+        )
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn level(&self) -> Option<u8> {
+        match *self {
+            Self::None | Self::Lz4 | Self::Lz4Auto => None,
+            Self::Lzma(l) | Self::LzmaAuto(l) => Some(l.level()),
+            Self::Zlib(l) | Self::ZlibAuto(l) => Some(l.level()),
+            Self::Zstd(l) | Self::ZstdAuto(l) => Some(l.level()),
+        }
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn as_smol_str(&self) -> SmolStr {
+        let prefix = ['a', 'u', 't', 'o', ','].into_iter();
+        match *self {
+            Self::None => SmolStr::new_static("none"),
+            Self::Lz4 => SmolStr::new_static("lz4"),
+            Self::Lz4Auto => SmolStr::new_static("auto,lz4"),
+            Self::Zstd(l) => SmolStr::new_inline(l.as_ref()),
+            Self::ZstdAuto(l) => prefix.chain(l.as_ref().chars()).collect(),
+            Self::Zlib(l) => SmolStr::new_inline(l.as_ref()),
+            Self::ZlibAuto(l) => prefix.chain(l.as_ref().chars()).collect(),
+            Self::Lzma(l) => SmolStr::new_inline(l.as_ref()),
+            Self::LzmaAuto(l) => prefix.chain(l.as_ref().chars()).collect(),
+        }
+    }
+}
+
+impl core::fmt::Display for Compression {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.as_smol_str().fmt(f)
+    }
+}
+
+impl std::str::FromStr for Compression {
+    type Err = &'static str;
+
+    #[inline]
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "none" => Ok(Self::None),
+            "lz4" => Ok(Self::Lz4),
+            "auto,lz4" => Ok(Self::Lz4Auto),
+            "lzma" => Ok(Self::Lzma(Lzma::default())),
+            "auto,lzma" => Ok(Self::LzmaAuto(Lzma::default())),
+            "zlib" => Ok(Self::Zlib(Zlib::default())),
+            "auto,zlib" => Ok(Self::ZlibAuto(Zlib::default())),
+            "zstd" => Ok(Self::Zstd(Zstd::default())),
+            "auto,zstd" => Ok(Self::ZstdAuto(Zstd::default())),
+            c if c.starts_with("lzma,") => Ok(Self::Lzma(c.parse()?)),
+            c if c.starts_with("auto,lzma,") => Ok(c[5..].parse()?),
+            c if c.starts_with("zlib,") => Ok(Self::Zlib(c.parse()?)),
+            c if c.starts_with("auto,zlib,") => Ok(c[5..].parse()?),
+            c if c.starts_with("zstd,") => Ok(Self::Zstd(c.parse()?)),
+            c if c.starts_with("auto,zstd,") => Ok(c[5..].parse()?),
+            _ => Err("Invalid compression format"),
+        }
+    }
+}
+
+impl TryFrom<&toml::Value> for Compression {
+    type Error = &'static str;
+
+    #[inline]
+    fn try_from(value: &toml::Value) -> Result<Self, Self::Error> {
+        if let Some(s) = value.as_str() {
+            return s.parse();
+        }
+
+        let Some(table) = value.as_table() else {
+            return Err("Invalid compression format");
+        };
+
+        let Some(algorithm) = table.get("algorithm").and_then(toml::Value::as_str) else {
+            return Err("Missing algorithm key");
+        };
+
+        let level = table
+            .get("level")
+            .and_then(toml::Value::as_integer)
+            .and_then(|l| u8::try_from(l).ok());
+
+        let auto = table
+            .get("auto")
+            .and_then(toml::Value::as_bool)
+            .unwrap_or(false);
+
+        let comp = match (algorithm, auto, level) {
+            ("none", ..) => Self::None,
+            ("lz4", false, ..) => Self::Lz4,
+            ("lz4", true, ..) => Self::Lz4Auto,
+            ("lzma", false, Some(l)) => Self::Lzma(l.try_into()?),
+            ("lzma", true, Some(l)) => Self::LzmaAuto(l.try_into()?),
+            ("zlib", false, Some(l)) => Self::Zlib(l.try_into()?),
+            ("zlib", true, Some(l)) => Self::ZlibAuto(l.try_into()?),
+            ("zstd", false, Some(l)) => Self::Zstd(l.try_into()?),
+            ("zstd", true, Some(l)) => Self::ZstdAuto(l.try_into()?),
+            ("zstd", false, ..) => Self::Zstd(Zstd::default()),
+            ("zstd", true, ..) => Self::ZstdAuto(Zstd::default()),
+            ("lzma", false, ..) => Self::Lzma(Lzma::default()),
+            ("lzma", true, ..) => Self::LzmaAuto(Lzma::default()),
+            ("zlib", false, ..) => Self::Zlib(Zlib::default()),
+            ("zlib", true, ..) => Self::ZlibAuto(Zlib::default()),
+            _ => return Err("Invalid compression algorithm"),
+        };
+
+        Ok(comp)
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Lz4;
 
@@ -49,6 +185,7 @@ pub enum Zstd {
 
 impl Zstd {
     #[inline]
+    #[must_use]
     pub const fn level(&self) -> u8 {
         *self as u8
     }
@@ -176,6 +313,7 @@ pub enum Zlib {
 
 impl Zlib {
     #[inline]
+    #[must_use]
     pub const fn level(&self) -> u8 {
         *self as u8
     }
@@ -267,6 +405,7 @@ pub enum Lzma {
 
 impl Lzma {
     #[inline]
+    #[must_use]
     pub const fn level(&self) -> u8 {
         *self as u8
     }
@@ -283,16 +422,16 @@ impl AsRef<str> for Lzma {
     #[inline]
     fn as_ref(&self) -> &str {
         match *self {
-            Lzma::Level0 => "lzma,0",
-            Lzma::Level1 => "lzma,1",
-            Lzma::Level2 => "lzma,2",
-            Lzma::Level3 => "lzma,3",
-            Lzma::Level4 => "lzma,4",
-            Lzma::Level5 => "lzma,5",
-            Lzma::Level6 => "lzma,6",
-            Lzma::Level7 => "lzma,7",
-            Lzma::Level8 => "lzma,8",
-            Lzma::Level9 => "lzma,9",
+            Self::Level0 => "lzma,0",
+            Self::Level1 => "lzma,1",
+            Self::Level2 => "lzma,2",
+            Self::Level3 => "lzma,3",
+            Self::Level4 => "lzma,4",
+            Self::Level5 => "lzma,5",
+            Self::Level6 => "lzma,6",
+            Self::Level7 => "lzma,7",
+            Self::Level8 => "lzma,8",
+            Self::Level9 => "lzma,9",
         }
     }
 }
@@ -337,144 +476,6 @@ impl TryFrom<u8> for Lzma {
             9 => Self::Level9,
             _ => return Err("invalid lzma compression level"),
         })
-    }
-}
-
-#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
-pub enum Compression {
-    None,
-    #[default]
-    Lz4,
-    Lz4Auto,
-    Zstd(Zstd),
-    ZstdAuto(Zstd),
-    Zlib(Zlib),
-    ZlibAuto(Zlib),
-    Lzma(Lzma),
-    LzmaAuto(Lzma),
-}
-
-impl Compression {
-    #[inline]
-    pub const fn is_none(&self) -> bool {
-        matches!(self, Self::None)
-    }
-
-    #[inline]
-    pub const fn is_auto(&self) -> bool {
-        matches!(
-            self,
-            Self::Lz4Auto | Self::LzmaAuto(..) | Self::ZstdAuto(..) | Self::ZlibAuto(..)
-        )
-    }
-
-    #[inline]
-    pub const fn level(&self) -> Option<u8> {
-        match *self {
-            Self::None | Self::Lz4 | Self::Lz4Auto => None,
-            Self::Lzma(l) | Self::LzmaAuto(l) => Some(l.level()),
-            Self::Zlib(l) | Self::ZlibAuto(l) => Some(l.level()),
-            Self::Zstd(l) | Self::ZstdAuto(l) => Some(l.level()),
-        }
-    }
-
-    #[inline]
-    pub fn as_smol_str(&self) -> SmolStr {
-        let prefix = ['a', 'u', 't', 'o', ','].into_iter();
-        match *self {
-            Self::None => SmolStr::new_static("none"),
-            Self::Lz4 => SmolStr::new_static("lz4"),
-            Self::Lz4Auto => SmolStr::new_static("auto,lz4"),
-            Self::Zstd(l) => SmolStr::new_inline(l.as_ref()),
-            Self::ZstdAuto(l) => prefix.chain(l.as_ref().chars()).collect(),
-            Self::Zlib(l) => SmolStr::new_inline(l.as_ref()),
-            Self::ZlibAuto(l) => prefix.chain(l.as_ref().chars()).collect(),
-            Self::Lzma(l) => SmolStr::new_inline(l.as_ref()),
-            Self::LzmaAuto(l) => prefix.chain(l.as_ref().chars()).collect(),
-        }
-    }
-}
-
-impl core::fmt::Display for Compression {
-    #[inline]
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.as_smol_str().fmt(f)
-    }
-}
-
-impl std::str::FromStr for Compression {
-    type Err = &'static str;
-
-    #[inline]
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "none" => Ok(Self::None),
-            "lz4" => Ok(Self::Lz4),
-            "auto,lz4" => Ok(Self::Lz4Auto),
-            "lzma" => Ok(Self::Lzma(Lzma::default())),
-            "auto,lzma" => Ok(Self::LzmaAuto(Lzma::default())),
-            "zlib" => Ok(Self::Zlib(Zlib::default())),
-            "auto,zlib" => Ok(Self::ZlibAuto(Zlib::default())),
-            "zstd" => Ok(Self::Zstd(Zstd::default())),
-            "auto,zstd" => Ok(Self::ZstdAuto(Zstd::default())),
-            c if c.starts_with("lzma,") => Ok(Self::Lzma(c.parse()?)),
-            c if c.starts_with("auto,lzma,") => Ok(c[5..].parse()?),
-            c if c.starts_with("zlib,") => Ok(Self::Zlib(c.parse()?)),
-            c if c.starts_with("auto,zlib,") => Ok(c[5..].parse()?),
-            c if c.starts_with("zstd,") => Ok(Self::Zstd(c.parse()?)),
-            c if c.starts_with("auto,zstd,") => Ok(c[5..].parse()?),
-            _ => Err("Invalid compression format"),
-        }
-    }
-}
-
-impl TryFrom<&toml::Value> for Compression {
-    type Error = &'static str;
-
-    #[inline]
-    fn try_from(value: &toml::Value) -> Result<Self, Self::Error> {
-        if let Some(s) = value.as_str() {
-            return s.parse();
-        }
-
-        let Some(table) = value.as_table() else {
-            return Err("Invalid compression format");
-        };
-
-        let Some(algorithm) = table.get("algorithm").and_then(toml::Value::as_str) else {
-            return Err("Missing algorithm key");
-        };
-
-        let level = table
-            .get("level")
-            .and_then(toml::Value::as_integer)
-            .and_then(|l| u8::try_from(l).ok());
-
-        let auto = table
-            .get("auto")
-            .and_then(toml::Value::as_bool)
-            .unwrap_or(false);
-
-        let comp = match (algorithm, auto, level) {
-            ("none", ..) => Self::None,
-            ("lz4", false, ..) => Self::Lz4,
-            ("lz4", true, ..) => Self::Lz4Auto,
-            ("lzma", false, Some(l)) => Self::Lzma(l.try_into()?),
-            ("lzma", true, Some(l)) => Self::LzmaAuto(l.try_into()?),
-            ("zlib", false, Some(l)) => Self::Zlib(l.try_into()?),
-            ("zlib", true, Some(l)) => Self::ZlibAuto(l.try_into()?),
-            ("zstd", false, Some(l)) => Self::Zstd(l.try_into()?),
-            ("zstd", true, Some(l)) => Self::ZstdAuto(l.try_into()?),
-            ("zstd", false, ..) => Self::Zstd(Zstd::default()),
-            ("zstd", true, ..) => Self::ZstdAuto(Zstd::default()),
-            ("lzma", false, ..) => Self::Lzma(Lzma::default()),
-            ("lzma", true, ..) => Self::LzmaAuto(Lzma::default()),
-            ("zlib", false, ..) => Self::Zlib(Zlib::default()),
-            ("zlib", true, ..) => Self::ZlibAuto(Zlib::default()),
-            _ => return Err("Invalid compression algorithm"),
-        };
-
-        Ok(comp)
     }
 }
 
